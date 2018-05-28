2d834ec41879af22d18c19f64a0cdef461c30318
David Holovii <mirage.present@gmail.com>
2018-05-28 14:58:52 +0300

    Login form

---
+++ front/src/app/screens/Auth/Login/components/LoginForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-card>
+        <v-card-title primary-title>
+            <h3 class="headline mb-0">Login</h3>
+        </v-card-title>
+        <v-container>
+            <v-form>
+                <v-text-field
+                  v-model="login"
+                  label="Username"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  required
+                  type="password"
+                ></v-text-field>
+            </v-form>
+        </v-container>
+        <v-card-actions class="login-form__actons">
+            <a href="">Forgot the password?</a>
+            <v-spacer></v-spacer>
+            <v-btn
+                class="blue"
+                dark
+                >
+                Enter <v-icon>keyboard_arrow_right</v-icon>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+  export default{
+      data() {
+          return {
+              login: '',
+              password: '',
+          };
+      }
+  }
+</script>
+<style>
+    .card__actions.login-form__actons{
+        padding: 16px
+    }
+</style>
--- front/src/app/screens/Auth/Login/components/LoginScreen.vue
+++ front/src/app/screens/Auth/Login/components/LoginScreen.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-layout row wrap>
-    <v-card sm8></v-card>
-  </v-layout>
+    <v-layout row wrap>
+        <v-flex
+            md4
+            sm6
+            xs12
+            offset-md4
+            offset-sm3
+            >
+            <login-form/>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
+import LoginForm from './LoginForm'
 export default {
+  components: {
+    LoginForm
+  }
 }
 </script>
 
